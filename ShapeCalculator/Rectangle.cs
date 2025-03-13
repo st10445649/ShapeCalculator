0b9e2b4370cda3e92fc523c9bd65429e2c1675db
ST10445649 <ST10445649@vcconnect.edu.za>
2025-03-13 14:43:06 +0200

    Rectangle class completed

--- ShapeCalculator/Rectangle.cs
+++ ShapeCalculator/Rectangle.cs
@@ -1,13 +1,24 @@
 namespace ShapeCalculator{
-    internal class Rectangle: Shape{
+    class Rectangle:Shape, Area 
+    {
+        public int ShapeLength {get; set; }
+        public int ShapeWidth {get; set; }
 
-        public string ShapeLength {get; set; }
-        public string ShapeWidth {get; set; }
-
-        public Rectangle (string shapeLength, string shapeWidth){
+        public Rectangle(string name, int shapeLength, int shapeWidth): base(name) {
             this.ShapeLength = shapeLength;
             this.ShapeWidth = shapeWidth;
         }
+
+        public  int CalculateArea(){
+           int area= ShapeLength * ShapeWidth;
+           return area;
+        }
+
+        public override void Display()
+    {
+        base.Display();
+        Console.WriteLine($"Length: {ShapeLength}, Width: {ShapeWidth}, Area: {CalculateArea()}");
+    }
 
     }
 }
